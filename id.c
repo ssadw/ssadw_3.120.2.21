50a8c657d88e6e9d48edc06857bd7477953154b8
vladimir <ssadww@gmail.com>
2024-04-02 11:39:00 +0700

    Программа с комментариями в исходном коде

---
+++ id.c
@@ -0,0 +1,19 @@
+//подключение библиотек
+#include <sys/types.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+//главная функция программы
+int main()
+{
+	uid_t userid;
+	gid_t groupid;
+//системный вызов getuid()
+	userid = getuid();
+//системный вызов getgid()
+	groupid = getgid();
+	printf("Ид. пользователя: %d\n", userid);
+	printf("Ид. группы пользователя: %d\n", groupid);
+//возврат значения 0 из главной функции
+	return 0;
+}
