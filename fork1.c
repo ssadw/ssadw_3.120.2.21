93b6fe5673c0f4b21ed3e106eee7123b24f91799
vladimir <ssadww@gmail.com>
2024-04-09 11:03:00 +0700

    Добавлены разные действия для процесса-родителя и для процесса-ребенка

--- fork1.c
+++ fork1.c
@@ -1,10 +1,8 @@
 //программа для иллюстрации одинаковой работы процесса-родителя и процесса-ребенка
-
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
-
 int main()
 {
 	pid_t procid, parentid; //идентификаторы процессов
@@ -15,5 +13,21 @@ int main()
 	procid = getpid(); //получение идентификатора текущего поцесса
 	parentid = getppid(); //получение идентификатора родительского процесса
 	printf("Ид. процесса: %d, ид. родителя %d, значение переменной a: %d\n", procid, parentid, a);
+	if (result < 0){
+		printf("Ошибка при выполнении fork()\n");
+		exit(-1);
+	}
+	else if (result == 0){
+		a = a + 1;
+		procid = getpid(); //получение идентификатора текущего поцесса
+		parentid = getppid(); //получение идентификатора родительского процесса
+		printf("Ид. процесса: %d, ид. родителя %d, значение переменной a: %d\n", procid, parentid, a);
+	}
+	else {
+		a = a + 555;
+		procid = getpid(); //получение идентификатора текущего поцесса
+        	parentid = getppid(); //получение идентификатора родительского процесса
+        	printf("Ид. процесса: %d, ид. родителя %d, значение переменной a: %d\n", procid, parentid, a);
+	}
 	return 0;
 }
