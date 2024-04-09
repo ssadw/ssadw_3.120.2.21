20d49e9edbabe397bea5f3695733f59c8012a9d5
vladimir <ssadww@gmail.com>
2024-04-09 12:39:00 +0700

    Разделен процесс работы программы в зависимости от кол-ва аргументов

--- cmdargs.c
+++ cmdargs.c
@@ -1,20 +1,26 @@
 #include <sys/types.h>
-#include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
-
-int main(int argc, char *argv[], char *envp[])
-{
+#include <unistd.h>
+int main (int argc, char *argv[], char *envp[] ) {
 	int i;
-	printf("Количество аргументов: %d\n\n", argc);
-	for(i=0; i<argc; i++) {
+       	printf("Количество аргументов: %d\n\n", argc);		
+	 if (argc<1) {
+		printf("ошибка\n");
+		exit(-1);
+ 	 }
+	 else if (argc ==1) {
+		while (envp[i]!=NULL) {
+	                printf("%s\n", envp[i]);
+	                i++;
+                }
+
+         }
+	 else{
+		for (i=0; i<argc; i++) {
 		printf("Аргумент %d: %s\n", i, argv[i]);
-	}
-	printf("\n");
-	i=0;
-	while (envp[i]!=NULL) {
-		printf("%s\n", envp[i]);
-		i++;
-	}
-        return 0;
+		}
+       	}
+
+	return 0;
 }
