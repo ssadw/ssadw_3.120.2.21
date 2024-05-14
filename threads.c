5a5bac6347f2354a4742d987c7135ac72bbd9878
vladimir <ssadww@gmail.com>
2024-05-14 13:30:00 +0700

    Добавлены комментарии в программу, работающую с threads

--- threads.c
+++ threads.c
@@ -1,29 +1,30 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-int a = 0;
+int a = 0; //инициализация глобальной статической переменной, доступной из каждого треда
+//функция, выполняющаяся в рамках второго треда
 void *mythread(void *dummy)
 {
-	pthread_t mythid;
+	pthread_t mythid; //идентификатор треда - для каждого имеет свое значение
 	mythid = pthread_self();
 	a = a+1;
-	printf("Thread %ld, Результат вычисления a = %d\n", mythid, a);
+	printf("Thread %ld, Результат вычисления a = %d\n", mythid, a); //печать идентификатора текущего треда и результата и значения глобальной переменной 
 	return NULL;
 }
 int main()
 {
 	pthread_t thid, mythid;
 	int result;
-	result = pthread_create(&thid, (pthread_attr_t *)NULL, mythread, NULL);
+	result = pthread_create(&thid, (pthread_attr_t *)NULL, mythread, NULL); //создание нового треда
 	if(result != 0)
 	{
 		printf("Ошибка при создании нового thread, возвращаемое значение = %d\n", result);
 		exit(-1);
 	}
 	printf("Thread создан, thid = %ld\n", thid);
-	mythid = pthread_self();
+	mythid = pthread_self(); //сохраняем в данной переменной идентификатор главного треда
 	a = a+1;
 	printf("Thread %ld, Результат вычисления a = %d\n", mythid, a);
-	pthread_join(thid, (void **)NULL);
+	pthread_join(thid, (void **)NULL); //ожидание завершения порожденного треда
 	return 0;
 }
