a7c584c429a072380c0225cdf6bacb7acd93f733
vladimir <ssadww@gmail.com>
2024-05-14 12:04:00 +0700

    Добавили программы для иллюстрации работы семафоров

---
+++ sems1.c
@@ -0,0 +1,32 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int semid;
+	char pathname[] = "forftok.ipc";
+	key_t key;
+	struct sembuf mybuf;
+	if((key = ftok(pathname, 0)) < 0)
+	{
+		printf("Не удалось сгенерировать ipc-ключ\n");
+		exit(-1);
+	}
+	if ((semid = semget(key, 1, 0666 | IPC_CREAT)) < 0)
+	{
+		printf("Не удалось получить доступ к массиву семафоров\n");
+		exit(-1);
+	}
+	mybuf.sem_op = 1;
+	mybuf.sem_flg = 0;
+	mybuf.sem_num = 0;
+	if(semop(semid, &mybuf, 1) < 0)
+	{
+		printf("Не удалось выполнить операцию над семафором 0\n");
+		exit(-1);
+	}
+	printf("Условие установлено\n");
+	return 0;
+}
