c62160c6da3d9237fe6de600cf853dba4e5897e4
vladimir <ssadww@gmail.com>
2024-05-21 11:43:00 +0700

    Добавили в справочную систему возможность выхода из программы

--- Help.java
+++ Help.java
@@ -4,6 +4,8 @@ class Help
 	throws java.io.IOException
 	{
 		char choice, ignore;
+
+		for(;;) {
 		do 
 		{
 			System.out.println("Справочная система:");
@@ -12,14 +14,16 @@ class Help
 			System.out.println(" 3. for");
 			System.out.println(" 4. while");
 			System.out.println(" 5. do-while");
+			System.out.println("q - выход из программы");
 			System.out.println("Выберите подраздел: ");
 			choice = (char) System.in.read();
 			do 
 			{
 				ignore = (char) System.in.read();
 			} while (ignore != '\n');
-		} while (choice < '1' | choice > '5');
+		} while (choice < '1' | choice > '5' & choice !='q');
 
+		if(choice == 'q') break;
 		System.out.println("\n");
 
 		switch (choice)
@@ -41,18 +45,20 @@ class Help
 			case '3':
                                 System.out.println("Цикл for:\n");
                                 System.out.println("for(инициализация; условие; итерация)");
-                                System.out.println("операторы;");
+                                System.out.println(" операторы;");
                                 break;
 			case '4':
 				System.out.println("Цикл wile:\n");
-                                System.out.println("условие) операторы");
+                                System.out.println("while(условие) операторы");
                                 break;
 			case '5':
                                 System.out.println("Цикл do-while:\n");
                                 System.out.println("do {");
-                                System.out.println("операторы");
+                                System.out.println("	операторы");
                                 System.out.println("} while(условие);");
                                 break;
+		}
+		System.out.println("");
 		}
 	}
 }
