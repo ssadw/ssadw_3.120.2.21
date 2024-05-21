9ed217d98d12d6de54bf9db0eb6c5bf6e6891944
vladimir <ssadww@gmail.com>
2024-05-21 11:23:00 +0700

    Добавили в справочную систему описание циклов

--- Help.java
+++ Help.java
@@ -4,19 +4,21 @@ class Help
 	throws java.io.IOException
 	{
 		char choice, ignore;
-
 		do 
 		{
 			System.out.println("Справочная система:");
 			System.out.println(" 1. if");
 			System.out.println(" 2. switch");
+			System.out.println(" 3. for");
+			System.out.println(" 4. while");
+			System.out.println(" 5. do-while");
 			System.out.println("Выберите подраздел: ");
 			choice = (char) System.in.read();
 			do 
 			{
 				ignore = (char) System.in.read();
 			} while (ignore != '\n');
-		} while (choice < '1' | choice > '2');
+		} while (choice < '1' | choice > '5');
 
 		System.out.println("\n");
 
@@ -36,8 +38,21 @@ class Help
 				System.out.println("	...");
 				System.out.println("}");
                         	break;
-			default:
-				System.out.print("Неверный номер подраздела");
+			case '3':
+                                System.out.println("Цикл for:\n");
+                                System.out.println("for(инициализация; условие; итерация)");
+                                System.out.println("операторы;");
+                                break;
+			case '4':
+				System.out.println("Цикл wile:\n");
+                                System.out.println("условие) операторы");
+                                break;
+			case '5':
+                                System.out.println("Цикл do-while:\n");
+                                System.out.println("do {");
+                                System.out.println("операторы");
+                                System.out.println("} while(условие);");
+                                break;
 		}
 	}
 }
