1800a00a46bc76c7f0f96595a8be7f3e1f13dc3a
vladimir <ssadww@gmail.com>
2024-06-04 11:31:00 +0700

    Добавлена пузырьковая сортировка массива

--- Array.java
+++ Array.java
@@ -9,5 +9,37 @@ class Array
 			nums[i] = i;
 		for(i = 0; i < 10; i++)
 			System.out.println("Элемент [" + i + "]: " + nums[i]);
+		//Альтернативный способ инициализации массива
+		int nums2[] = {99, -50, -232, 45, -34, 34, -67, 12, 555, -4};
+		int min, max;
+		//поиск максимального и минимального значений
+		min = max = nums2[0];
+		for (i = 1; i < 10; i++)
+		{
+			if(nums2[i] < min) min = nums2[i];
+			if(nums2[i] > max) max = nums2[i];
+		}
+		System.out.println("Минимальное значение в массиве: " + min + "\nМаксимальное значение в массиве: " + max);
+		//Пузырьковая сортировка в массиве
+		int a, b, t;
+		int size = 10;
+		System.out.println("Массив до сортировки:");
+		for (i = 0; i < size; i++)
+			System.out.println(" " + nums2[i]);
+		System.out.println();
+
+		for(a=1; a < size; a++)
+			for(b=size-1; b >= a; b--)
+			{
+				if(nums2[b-1] > nums2[b]) 
+				{
+					t = nums2[b-1];
+					nums2[b-1] = nums2[b];
+					nums2[b] = t;
+				}
+			}
+		System.out.println("Массив после сортировки:");
+                for (i = 0; i < size; i++)
+                        System.out.println(" " + nums2[i]);
 	}
 }
