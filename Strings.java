7840f7b7d1793cd546b8a03939200ce85ba321fc
vladimir <ssadww@gmail.com>
2024-06-04 13:04:00 +0700

    Добавлена программа демонстрирующая наследование

--- Strings.java
+++ Strings.java
@@ -7,11 +7,30 @@ class Strings
 		String str3 = "Третья строка";
 		int result, idx;
 		char ch;
-
 		System.out.println("Длина строки str1: " + str1.length());
 		//посимвольный вывод строки на экран
 		for(int i = 0; i < str1.length(); i++)
 			System.out.print(str1.charAt(i));
 		System.out.println();
+		if(str1.equals(str2))
+			System.out.println("str1 и str2 эквивалентны");
+		else
+			System.out.println("str1 и str2 не эквивалентны");
+		if(str1.equals(str3))
+                        System.out.println("str1 и str3 эквивалентны");
+                else
+                        System.out.println("str1 и str3 не эквивалентны");
+		result = str1.compareTo(str3);
+		if(result == 0)
+			System.out.println("str1 и str3 равны");
+		else if(result < 0)
+			System.out.println("str1 меньше, чем str3");
+		else
+			System.out.println("str1 больше, чем str3");
+		String str4 = "Строка для поиска подстроки. Значения: подстрока 1, подстрока 2, подстрока 3";
+		idx = str4.indexOf("подстрока");
+		System.out.println("Индекс первого вхождения слова \"подстрока\": " + idx);
+		idx = str4.lastIndexOf("подстрока");
+                System.out.println("Индекс последнего вхождения слова \"подстрока\": " + idx);
 	}
 }
